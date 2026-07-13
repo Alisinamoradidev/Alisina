95cccbf2ad44ddc31ef1bd7f8eee9fc70755db8d
Mustafa8801 <mustafamoradi2022@gmail.com>
2026-07-13 19:59:51 +0430

    Add service worker for PWA install prompt

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'primenest-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/config.js',
+  '/images/logo.jpeg',
+  '/images/icon-192.jpeg',
+  '/images/icon-512.jpeg',
+  '/site.webmanifest',
+  '/404.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
